bffda367c7f25f6d3d1dc4c39d6bd8d4095053a9
Flexflo42 <serenityflix@proton.me>
2026-03-27 13:14:53 +0100

    implemented basic miracle sort algorithm

--- miracleSort.py
+++ miracleSort.py
@@ -1,1 +1,16 @@
-init 
+import time
+
+intList = [3, 7, 21, 9, 4, 12, 102, 44, 2, 98, 67, 42]
+aMiracleHappened = [2, 3, 4]
+
+def miracleSort(intList):
+    while True:
+        if all(intList[i] <= intList[i+1] for i in range(len(intList)-1)):
+            print("The List is sorted")
+            exit()
+        else:
+            print("The List is not sorted. We should wait for divine intervention")
+            time.sleep(2)
+
+miracleSort(aMiracleHappened)
+
