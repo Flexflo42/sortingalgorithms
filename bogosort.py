755a7e67332dee1318b0d2f7069546b0f202f8c1
Flexflo42 <serenityflix@proton.me>
2026-03-27 13:41:49 +0100

    implemented step counter and print statement

--- bogosort.py
+++ bogosort.py
@@ -1,14 +1,16 @@
 import random
 
-intList = [21, 9, 4, 12, 102, 44, 2, 98, 67, 42]
-#
+# good luck
+intList = [4, 12, 102, 44, 2, 98, 67, 42]
 
 def bogosort(intList):
+    attempts = 0
     while True:
         if all(intList[i] <= intList[i+1] for i in range(len(intList)-1)):
             print("The List is sorted")
             return intList
         random.shuffle(intList)
-        print("Everday I'm shuffling")
+        attempts += 1
+        print(f"Everday I'm shuffling #{attempts}: {intList}")
 
 bogosort(intList)
