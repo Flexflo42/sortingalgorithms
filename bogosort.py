fa15b13e6d569b5bed50e7e2d9743fb33c53703e
Flexflo42 <serenityflix@proton.me>
2026-03-27 13:35:52 +0100

    implemented basic bogosort

--- bogosort.py
+++ bogosort.py
@@ -1,1 +1,14 @@
-init
+import random
+
+intList = [21, 9, 4, 12, 102, 44, 2, 98, 67, 42]
+#
+
+def bogosort(intList):
+    while True:
+        if all(intList[i] <= intList[i+1] for i in range(len(intList)-1)):
+            print("The List is sorted")
+            return intList
+        random.shuffle(intList)
+        print("Everday I'm shuffling")
+
+bogosort(intList)
