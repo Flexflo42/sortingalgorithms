c3763a2d6f83d860091bd60c5efd87676686f462
Flexflo42 <serenityflix@proton.me>
2026-03-27 14:12:22 +0100

    added bogosort to readme and calculate chance function to bogosort.py

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Simply the worst sorting algorithms you can imagine.
 
 ## Miracle Sort
 Sorting algorithm based on the idea of waiting till something happens. When you wait enough everything will always sort itself. Needs strong faith and lucky Bitflips on RAM that doesnt automatically corrects errors. 
+
+## Bogosort
+Unstable sorting algorithm that randomizes the indexes of elements till the input is sorted. Every list has n! permutations so the chance for success in a single execution of the algorithm is 1/n!. The chance for one success in k amount of algorithm runs is 1 - (1 - 1/n!)^k where k is the amount of runs. 
--- bogosort.py
+++ bogosort.py
@@ -1,16 +1,32 @@
 import random
+import math
+import time
 
 # good luck
 intList = [4, 12, 102, 44, 2, 98, 67, 42]
+n = len(intList)
+
+def calculateChance(k):
+    k = int(k)
+    x = len(intList)
+    factorial = math.factorial(x)
+    chance = 1 - (1 - x/factorial)**k
+    return chance
 
 def bogosort(intList):
     attempts = 0
     while True:
         if all(intList[i] <= intList[i+1] for i in range(len(intList)-1)):
             print("The List is sorted")
-            return intList
+            return intList, attempts
         random.shuffle(intList)
         attempts += 1
         print(f"Everday I'm shuffling #{attempts}: {intList}")
 
-bogosort(intList)
+userGuess = input(f"How many attempts do you think will a list with {n} Elements take? : ")
+chance = calculateChance(userGuess)
+print(f"the chance of this being a success is {chance}")
+time.sleep(10)
+print("LETS ROLL")
+
+sorted_list, attempts = bogosort(intList)
